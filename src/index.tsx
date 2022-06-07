9e5f706b35ad0375048ebb2ea5717c222d6234f3
sveta <Sunkovasvetlana@gmail.com>
2022-06-07 12:14:28 +0300

    start project

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import App from './n1-main/m1-ui/App';
 import reportWebVitals from './reportWebVitals';
+import {HashRouter} from "react-router-dom";
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
+      <HashRouter>
     <App />
+      </HashRouter>
   </React.StrictMode>
 );
 
---
+++ src/n1-main/m1-ui/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+import {Routes} from 'react-router-dom';
+//import {Route} from 'react-router-dom';
+
+function App() {
+  return (
+    <div className="App">
+<Routes>
+  {/*<Route path={'/'} element={<Header/>}/>*/}
+  {/*<Route path={'/'} element={<Main/>}/>*/}
+</Routes>
+    </div>
+  );
+}
+
+export default App;
