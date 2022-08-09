b242b3817a2298c792c55e3a2031cbab7caea2dc
sveta-confeta <_svet_@tut.by>
2022-08-09 21:32:15 +0300

    куÐDeleteCardModal

---
+++ src/components/cards/modals/DeleteCardModal.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import {useAppDispatch} from "../../../bll/state";
+import {BasicModal} from "../../../common/modal/Modal";
+import {deleteCardTC} from "../../../bll/cardsReducer";
+
+
+type DeleteCardType = {
+    cardQuestion: string
+    cardId: string
+    packId: string
+    isOpenModal: boolean
+    setIsOpenModal: (value: boolean) => void
+}
+
+export const DeleteCardModal: React.FC<DeleteCardType> = React.memo(({
+                                                                         cardId,
+                                                                         packId,
+                                                                         cardQuestion,
+                                                                         isOpenModal,
+                                                                         setIsOpenModal
+                                                                     }) => {
+    const dispatch = useAppDispatch()
+
+    const deleteCard = () => {
+        dispatch(deleteCardTC(cardId, packId))
+    }
+
+    return (
+        <BasicModal
+            title={'Delete Card'}
+            buttonName={'Delete'}
+            handleOperation={deleteCard}
+            isOpenModal={isOpenModal}
+            setIsOpenModal={setIsOpenModal}
+        >
+            <div>Do you really want to remove card with question <b>{cardQuestion}</b>?</div>
+            <div>The card will be removed.</div>
+        </BasicModal>
+    );
+});
